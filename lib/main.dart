2140cccc2a0a1548fdd51618a2d3c53183de0235
Dong Darong <148829792+DongDarong@users.noreply.github.com>
2025-07-07 08:37:32 +0700

    modified code UI

--- lib/main.dart
+++ lib/main.dart
@@ -133,6 +133,7 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  SizedBox(height: 30,),
                   // Top -----------------------------------------------------------
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
@@ -307,7 +308,7 @@ class _HomePageState extends State<HomePage> {
                       crossAxisCount: 3,
                       mainAxisSpacing: 15,
                       crossAxisSpacing: 15,
-                      childAspectRatio: 1.3,
+                      childAspectRatio: 1,
                     ),
                     itemCount: data.length,
                     itemBuilder: (context, index) {
